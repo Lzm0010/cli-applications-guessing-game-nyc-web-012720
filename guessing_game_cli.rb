ba2f3a19edf953310a5fb0650614d4e1034856e3
Lee McCormick <lzm0010@gmail.com>
2020-01-30 15:47:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,6 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   random_number = rand(6) + 1
   user_guess = gets.chomp
+  binding.irb
   if user_guess == 'exit'
     puts "Goodbye!"
   else

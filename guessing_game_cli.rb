fc137986e11f0c496ff54b30ee308f40d00da9c5
Lee McCormick <lzm0010@gmail.com>
2020-01-30 15:32:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,5 +6,5 @@ def run_guessing_game
   if user_guess == random_number
     puts "You guessed the correct number!"
   else
-    puts "Sorry! The computer guessed #{random_number}""
+    puts "Sorry! The computer guessed #{random_number}"
 end

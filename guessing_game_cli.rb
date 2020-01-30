dc5025afdf219637d0311fab2639fe07a98102ce
Lee McCormick <lzm0010@gmail.com>
2020-01-30 15:32:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   user_guess = gets
   if user_guess == random_number
-    puts "You guessed the correct number"
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{random_number}""
 end

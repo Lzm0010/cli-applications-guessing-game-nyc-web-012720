081356f12d86d7ee2b274be6ddb916d8415a8968
Lee McCormick <lzm0010@gmail.com>
2020-01-30 15:42:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,6 +4,7 @@ def run_guessing_game
   random_number = rand(1..6) + 1
   user_guess = gets.chomp
   if user_guess == 'exit'
+    puts "Goodbye!"
     if user_guess == random_number
       puts "You guessed the correct number!"
     else
